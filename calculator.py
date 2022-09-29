14e7238d147602aef60e62667b946baa34466580
azizosharke <abdelaza@tcd.ie>
2022-09-29 20:46:34 +0100

    finished the interface def calculation

--- calculator.py
+++ calculator.py
@@ -6,6 +6,16 @@ def check_if_is_number(expression):
         return True
     except ValueError:
         return False
+def calculator():
+    calc = input("Enter your Expression Here: ")
+    # remove white spaces
+    calc = calc.replace(' ', '')
+    # check for invalid expression
+    for expression in calc:
+        if expression not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*']:
+            print('Invalid Expression: ' + expression)
+            exit()
+
 def operation(num1, operator, num2):
     if operator == "+":
         return str(int(num1) + int(num2))
